bcda7c4e72b259e9a0d212bb3deb5f6bb81399b4
Niko Tobiasson <niko.tobiasson@reaktor.com>
2023-03-03 15:34:40 +0200

    Prettier, lint and github actions initialized

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react'
 
 export interface ButtonProps {
-  label: string;
+  label: string
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
-};
+const Button = (props: ButtonProps) => <button>{props.label}</button>
 
-export default Button;
+export default Button
